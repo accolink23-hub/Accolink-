041ff2619c9d8e366249e2e2a38b83d2fc2bdc1a
accolink23-hub <accolink23@gmail.com>
2026-02-07 23:34:48 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 // Replace with your Supabase project info
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_KEY = "YOUR_PUBLIC_ANON_KEY";
+const SUPABASE_URL = "https://yrejueipcrcwmsseqqqz.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlyZWp1ZWlwY3Jjd21zc2VxcXF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0NTM4NzcsImV4cCI6MjA4NjAyOTg3N30.xDsmkrnTW_bCeCyWeZfge4GujuM34JfZ98dx_t1VFJY";
 
 const supabase = supabaseJs.createClient(SUPABASE_URL, SUPABASE_KEY);
