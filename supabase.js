cd339edfdd314ab6d5b4429ab3f7ea45f2e2543e
accolink23-hub <accolink23@gmail.com>
2026-02-07 23:29:09 +0200

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,5 @@
+// Replace with your Supabase project info
+const SUPABASE_URL = "YOUR_SUPABASE_URL";
+const SUPABASE_KEY = "YOUR_PUBLIC_ANON_KEY";
+
+const supabase = supabaseJs.createClient(SUPABASE_URL, SUPABASE_KEY);
