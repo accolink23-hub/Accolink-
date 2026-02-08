8bac362c6180908ce0a552f833a02ed0dfaa43d1
accolink23-hub <accolink23@gmail.com>
2026-02-08 05:03:02 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,8 @@
-// Replace with your Supabase project info
 const SUPABASE_URL = "https://yrejueipcrcwmsseqqqz.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlyZWp1ZWlwY3Jjd21zc2VxcXF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0NTM4NzcsImV4cCI6MjA4NjAyOTg3N30.xDsmkrnTW_bCeCyWeZfge4GujuM34JfZ98dx_t1VFJY";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlyZWp1ZWlwY3Jjd21zc2VxcXF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0NTM4NzcsImV4cCI6MjA4NjAyOTg3N30.xDsmkrnTW_bCeCyWeZfge4GujuM34JfZ98dx_t1VFJY ";
 
-const supabase = supabaseJs.createClient(SUPABASE_URL, SUPABASE_KEY);
+// IMPORTANT: use window.supabase
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
